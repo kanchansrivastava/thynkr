cc3e1f7fb53002fab9e3e6700129014f3fd26faa
Kanchan Srivastava <kanchansrivastava1991@gmail.com>
2025-08-06 18:08:53 +0530

    initial setup

---
+++ app/core/logging_config.py
@@ -0,0 +1,40 @@
+import logging
+from logging.config import dictConfig
+import os
+from logging.handlers import TimedRotatingFileHandler
+
+def setup_logging():
+    log_dir = os.getenv("LOG_DIR", "logs")
+    os.makedirs(log_dir, exist_ok=True)
+
+    log_file = os.path.join(log_dir, "app.log")  # base name (rotated automatically)
+
+    dictConfig({
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] [%(levelname)s] %(name)s: %(message)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S"
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "default",
+            },
+            "file": {
+                "class": "logging.handlers.TimedRotatingFileHandler",
+                "formatter": "default",
+                "filename": log_file,
+                "when": "midnight",            # Rotate at midnight
+                "backupCount": 7,              # Keep last 7 days of logs
+                "encoding": "utf-8",
+                "utc": True                    # Optional: use UTC time
+            },
+        },
+        "root": {
+            "level": "INFO",
+            "handlers": ["console", "file"]
+        },
+    })
---
+++ app/main.py
@@ -0,0 +1,28 @@
+# app/main.py
+from fastapi import FastAPI
+import uvicorn
+import os
+from dotenv import load_dotenv
+from app.core.logging_config import setup_logging
+import logging
+
+load_dotenv()  # Load values from .env into environment
+
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    logger.info("Root endpoint called")
+    return {"msg": "Hello! Welcome to Thynkr "}
+
+if __name__ == "__main__":
+    host = os.getenv("HOST", "127.0.0.1")
+    port = int(os.getenv("PORT", 8000))
+    reload = os.getenv("RELOAD", "false").lower() == "true"
+
+    uvicorn.run("app.main:app", host=host, port=port, reload=reload)
+
